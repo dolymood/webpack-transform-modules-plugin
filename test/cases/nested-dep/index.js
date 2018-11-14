a451f96d4aea2db3aa97410ef8bbdc2ada6ea883
candyJ <cuijing_1031@163.com>
2018-11-14 20:56:07 +0800

    [feat]collectTransformModules support dependencies modules

---
+++ test/cases/nested-dep/index.js
@@ -0,0 +1,13 @@
+import { index as a } from 'a'
+import b from 'b'
+import { index as c } from 'c'
+import { index as d } from 'd'
+import pkg from './package.json'
+
+export {
+  a,
+  b,
+  c,
+  d,
+  pkg
+}
--- test/integration/nested.js
+++ test/integration/nested.js
@@ -1,10 +1,23 @@
 var path = require('path')
 var webpack = require('webpack')
 var webpackConfig = require('../cases/nested/webpack.config')
+var webpackConfig2 = require('../cases/nested-dep/webpack.config')
 var expect = require('chai').expect
 
 describe('nested case', function () {
-  it('webpack options should be correct', function (done) {
+  var warns = []
+  var originConsoleWarn = null
+  beforeEach(function() {
+    console.warn = function (str) {
+      warns.push(str)
+    }
+  })
+  afterEach(function() {
+    console.warn = originConsoleWarn
+    warns = []
+    originConsoleWarn = null
+  })
+  it('plugin options', function (done) {
     webpack(webpackConfig, function (err, stats) {
       var compiler = stats.compilation.compiler
       var options = compiler.options
@@ -15,6 +28,25 @@ describe('nested case', function () {
       expect(ret.a).to.equal('nested a')
       expect(ret.b).to.equal('nested b, nested inner a, @dd nested inner a')
       expect(ret.pkg.name).to.equal('nested')
+      done()
+    })
+  })
+  it('nested dep', function (done) {
+    webpack(webpackConfig2, function (err, stats) {
+      var compiler = stats.compilation.compiler
+      var options = compiler.options
+      var rule = options.module.rules[0]
+      expect(rule.use.options.plugins.length)
+        .to.equal(1)
+      var ret = require('../cases/nested-dep/app.js')
+      expect(ret.a).to.equal('nested a')
+      expect(ret.b).to.equal('nested b, nested d, nested e, nested a@0.0.2')
+      expect(ret.c).to.equal('nested c')
+      expect(ret.d).to.equal('nested d')
+      expect(ret.pkg.name).to.equal('nested')
+      expect(warns.length).to.equal(2)
+      expect(warns[0]).to.equal('[webpack-transform-modules-plugin] There are multiple node modules configured "transformModules" with module: "a", in:/Users/didi/xiaoju/github/webpack-transform-modules-plugin/test/cases/nested-dep/node_modules/d')
+      expect(warns[1]).to.equal('[webpack-transform-modules-plugin] There are multiple node modules configured "transformModules" with module: "a", in:/Users/didi/xiaoju/github/webpack-transform-modules-plugin/test/cases/nested-dep/node_modules/b/node_modules/a')
       done()
     })
   })
