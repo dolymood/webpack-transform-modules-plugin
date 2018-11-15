4be173f9957cc48f93252278a8dc6b49ccacf91d
Candy.J <cuijing_1031@163.com>
2018-11-15 11:02:25 +0800

    [feat]collectTransformModules support dependencies modules (#4)
    * [feat]collectTransformModules support dependencies modules
    
    * [fix]fix test

---
+++ test/cases/nested-dep/webpack.config.js
@@ -0,0 +1,31 @@
+var path = require('path')
+var TransformModulesPlugin = require('../../../lib/index')
+
+module.exports = {
+  context: __dirname,
+  entry: {
+    app: path.resolve(__dirname, './index')
+  },
+  output: {
+    path: path.resolve(__dirname, './'),
+    library: 'normal',
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      }
+    ]
+  },
+  plugins: [
+    new TransformModulesPlugin()
+  ]
+}
